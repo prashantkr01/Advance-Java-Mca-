1899f9fd964afd9899ae77fab5ed1cc116cdd688
prashantkr01 <prashantkr085@gmail.com>
2026-03-25 15:56:48 +0530

    streamreduce

---
+++ streamreduce/streamred.java
@@ -0,0 +1,34 @@
+package streamreduce;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class streamred {
+    public static void main(String[] args) {
+    //    List<Integer> list=Arrays.asList(12,14,54,67,14,147,96);
+    //    long result=list.stream().reduce(1,(a,b)->a*b);
+    //    System.out.println(result);
+
+    //    List<Integer> list=Arrays.asList(12,14,54,67,14,147,96);
+    //    long result=list.stream().reduce(1,(a,b)->a>b?a:b);
+    //    System.out.println(result);
+
+    //    List<Integer> list=Arrays.asList(12,14,54,67,14,147,96);
+    //    long result=list.stream().reduce(0,(a,b)->a+1);
+    //    System.out.println(result);
+
+    //    List<Integer> list=Arrays.asList(11,22,33,44,55,66,77,88);
+    //    int result=list.stream().reduce(0,(a,b)->b%2==0?a+b:a);
+    //    System.out.println(result);
+    
+    //    List<String> list=Arrays.asList("Java","Stream","API","Reduce");
+    //    String result=list.stream().reduce("",(a,b)->a+b);
+    //    System.out.println(result);
+    
+         List<Integer> list=Arrays.asList(1,2,3,4,5,6);
+         int result=list.stream().reduce(0,(a,b)->b*b+a);
+         System.out.println(result);
+    
+
+    }
+}
