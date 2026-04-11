85018cfe19d46e277388a17e13739a8de60a4aff
prashantkr01 <prashantkr085@gmail.com>
2026-04-12 01:32:37 +0530

    questrAdd strdist class to demonstrate distinct and sorted elements in a list

---
+++ strred/strdistinct/strdist.java
@@ -0,0 +1,12 @@
+package strred.strdistinct;
+
+import java.util.Arrays;
+import java.util.List;  
+
+public class strdist {
+    public static void main(String[] args) {
+        List<Integer> list=Arrays.asList(10,5,10,7,5,3,8,3);
+        List<Integer> result=list.stream().distinct().sorted().toList();
+        System.out.println(result);
+    }
+}
